3dec847a0c4277e5bdc2ae36844e68ec7f37a7fa
Dan Lindeman <lindemda@gmail.com>
2021-04-26 10:06:07 -0400

    core counter module

---
+++ summer/lib/summer/application.ex
@@ -0,0 +1,20 @@
+defmodule Summer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Summer.Worker.start_link(arg)
+      # {Summer.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Summer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
