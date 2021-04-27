ef8989be72a5380f0b66c15d7f9730516acc03de
Dan Lindeman <lindemda@gmail.com>
2021-04-27 10:48:10 -0400

    parrot brought to life

--- parrot/lib/parrot/eraser.ex
+++ parrot/lib/parrot/eraser.ex
@@ -2,7 +2,15 @@ defmodule Parrot.Eraser do
   defstruct ~w[plan text]a
 
   def new(text, steps) do
-    # ...
+    length = String.length(text)
+    chunk_size = ceil(length / steps)
+
+    plan =
+      0..length
+      |> Enum.shuffle()
+      |> Enum.chunk_every(chunk_size)
+
+    %__MODULE__{plan: plan, text: text}
   end
 
   def erase(eraser) do
